3116705838aa5e3a242f22f7c4533b644d5c42a9
mperezpizarro <marioperezpizarro@gmail.com>
2022-10-04 12:13:55 +0200

    Generates passwords

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { BsFillMoonStarsFill, BsFillSunFill } from 'react-icons/bs'
 import PasswordForm from './components/PasswordForm'
 
 const App = () => {
   const [dark, setDark] = useState(false)
 
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches)
+      setDark(true)
+  }, [])
+
   return (
     <main className={dark ? 'dark' : ''}>
-      <div className='min-h-screen flex justify-center items-center bg-zinc-100 dark:bg-gray-700'>
+      <div className='transition ease-in-out min-h-screen flex justify-center items-center bg-zinc-100 dark:bg-gray-700'>
         <div className='max-w-sm text-sky-700 dark:text-sky-300 shadow-lg dark:shadow-[#ffffff22] rounded-xl px-6 py-8'>
           <div className='flex justify-center gap-x-3 mb-8'>
             <h2 className='font-bold uppercase text-2xl'>Password generator</h2>
--- src/components/PasswordForm.jsx
+++ src/components/PasswordForm.jsx
@@ -4,24 +4,45 @@ import { useState } from 'react'
 
 const PasswordForm = () => {
     const [range, setRange] = useState(8)
+    const [pw, setPw] = useState('')
+    const pwChars='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz@#$-_/%^&*+=!-'
 
     const handleSubmit = (e) =>{
         e.preventDefault()
+
+        let tmpPW=generatePW
+
+        setPw(tmpPW)
+    }
+
+    const generatePW = () =>{
+        let tmpPW=''
+
+        for(let i=0; i<range; i++){
+            let random = Math.floor(Math.random() * (pwChars.length - 1)) 
+            tmpPW += pwChars.substring(random, random+1)
+        }
+
+        return tmpPW
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <div className='shadow-md dark:shadow-[#ffffff22] rounded-lg px-4 py-3 flex justify-between gap-x-2'>
-                <input type="text" name="password" id="password" disabled='disabled' placeholder='Your password...' className='bg-transparent w-full' />
+                <input 
+                    type="text" name="password" id="password" disabled='disabled' 
+                    placeholder='Your password...' value={pw} 
+                    className='bg-transparent w-full text-gray-700 dark:text-gray-100' 
+                />
                 <button type='button' className='text-xl'>
                     <BsClipboard />
                 </button>
             </div>
             <div className='text-gray-700 dark:text-gray-100 my-6'>
-                <label htmlFor="strength">Password strength:</label>
+                <label htmlFor="strength">Password length:</label>
                 <div className='flex items-center gap-x-4 my-2'>
                     <input 
-                        type="range" name="strength" id="strength" 
+                        type="range" name="length" id="length" 
                         defaultValue={8} min={8} max={24} className='flex-1'
                         onChange={(e) => {setRange(e.currentTarget.value)}} 
                     />
