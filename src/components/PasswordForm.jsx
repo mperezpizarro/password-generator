af959e5812fa96a546ef35e5e5742e185f1ada6b
mperezpizarro <marioperezpizarro@gmail.com>
2022-10-04 12:40:27 +0200

    Can copy to clipboard

--- src/components/PasswordForm.jsx
+++ src/components/PasswordForm.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BsClipboard } from 'react-icons/bs'
 import { useState } from 'react'
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 
 const PasswordForm = () => {
     const [range, setRange] = useState(8)
@@ -26,6 +28,33 @@ const PasswordForm = () => {
         return tmpPW
     }
 
+    const success = () =>{
+        navigator.clipboard.writeText(pw)
+        Toastify({
+            text: "Copied to clipboard!",
+            duration: 2000,
+            gravity: "bottom",
+            position: "center",
+            style: {
+                background: 'linear-gradient(132deg, rgb(224, 235, 213) 0.00%, rgb(37, 148, 141) 100.00%',
+                color: 'black'
+            }
+        }).showToast()
+    }
+
+    const error = () =>{
+        Toastify({
+            text: "Can't copy to clipboard",
+            duration: 2000,
+            gravity: "bottom",
+            position: "center",
+            style: {
+                background: 'linear-gradient(132deg, rgb(114, 43, 54) 0.00%, rgb(238, 178, 123) 100.00%)',
+                color: 'black'
+            }
+        }).showToast()
+    }
+
     return (
         <form onSubmit={handleSubmit}>
             <div className='shadow-md dark:shadow-[#ffffff22] rounded-lg px-4 py-3 flex justify-between gap-x-2'>
@@ -34,7 +63,7 @@ const PasswordForm = () => {
                     placeholder='Your password...' value={pw} 
                     className='bg-transparent w-full text-gray-700 dark:text-gray-100' 
                 />
-                <button type='button' className='text-xl'>
+                <button type='button' className='text-xl' onClick={() => {pw != '' ? success() : error()}}>
                     <BsClipboard />
                 </button>
             </div>
